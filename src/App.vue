5d54dcfc9f7274fefe6df75bcdf191bf5a0214eb
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-01 18:56:36 -0500

    App.vue changes->Delete & change some shitty default comp

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+          <template>
+            <v-list-tile :to="{ name : 'home'}">
+              <v-list-tile-action>
+                <v-icon>home</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>
+                Inicio
+              </v-list-tile-title>
+            </v-list-tile>
+          </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="blue darken-3"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Sistema</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
       </v-btn>
     </v-toolbar>
-
     <v-content>
-      <router-view/>
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+            <router-view/>
+        </v-slide-y-transition>
+      </v-container>
     </v-content>
   </v-app>
 </template>
