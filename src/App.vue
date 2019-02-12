376100998682c33a9a8a47116caaa383bcbf0264
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-12 15:47:31 -0500

    Creating the component Categoria with a data-table  & adding it into routes

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
                 </v-list-tile-title>
               </v-list-tile-content>
             </v-list-tile>
-            <v-list-tile :to="{ name: ''}">
+            <v-list-tile :to="{ name: 'categorias'}">
               <v-list-tile-action>
                 <v-icon>table_chart</v-icon>
               </v-list-tile-action>
@@ -226,4 +226,3 @@ export default {
   }
 }
 </script>
-<!--
