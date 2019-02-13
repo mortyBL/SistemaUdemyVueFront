0e73cc9933da0bde98b1e39e666b5c86ee6e0ffc
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-13 13:29:04 -0500

    Deleting some default code & customizing data-table

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -2,77 +2,73 @@
   <v-layout align-start>
       <v-flex>
             <v-toolbar flat color="#b2dfdb">
-            <v-toolbar-title>Categorías</v-toolbar-title>
-            <v-divider
-                class="mx-2"
-                inset
-                vertical
-            ></v-divider>
-            <v-spacer></v-spacer>
-            <v-text-field class="text-xs-center" v-model="search" append-icon="search" label="Búsqueda" single-line hide-details></v-text-field>
-            <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-                <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
-                <v-card>
-                <v-card-title>
-                    <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
+              <v-toolbar-title>Categorías</v-toolbar-title>
+              <v-divider
+                  class="mx-2"
+                  inset
+                  vertical
+              ></v-divider>
+              <v-spacer></v-spacer>
+              <v-text-field class="text-xs-center" v-model="search" append-icon="search" label="Búsqueda" single-line hide-details></v-text-field>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="500px">
+                  <v-btn slot="activator" color="primary" dark class="mb-2">Nueva Categoría</v-btn>
+                  <v-card>
+                    <v-card-title class="justify-center">
+                        <span class="headline">{{ formTitle }}</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12 sm6 md4>
+                            <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                            <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                            <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                            <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md4>
+                            <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                        </v-container>
+                    </v-card-text>
 
-                <v-card-text>
-                    <v-container grid-list-md>
-                    <v-layout wrap>
-                        <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                    </v-container>
-                </v-card-text>
-
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-                    <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-                </v-card-actions>
-                </v-card>
-            </v-dialog>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+                        <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+                    </v-card-actions>
+                  </v-card>
+              </v-dialog>
             </v-toolbar>
+            <!-- DATA TABLE STARTS HERE -->
             <v-data-table
             :headers="headers"
             :items="categorias"
             :search="search"
-            class="elevation-1"
+            class="elevation-3"
             >
             <template slot="items" slot-scope="props">
-                  <td class="justify-left">
-                  <v-icon
-                      small
-                      class="mr-2"
-                      @click="editItem(props.item)"
-                  >
-                      edit
-                  </v-icon>
-                  <v-icon
-                      small
-                      @click="deleteItem(props.item)"
-                  >
-                      delete
-                  </v-icon>
+                  <td class="text-xs-center">
+                  <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
+                  <v-icon small @click="deleteItem(props.item)">delete</v-icon>
                   </td>
-                  <td>{{ props.item.nombre }}</td>
-                  <td>{{ props.item.descripcion }}</td>
-                  <td>{{ props.item.condicion }}</td>
+                  <td class="text-xs-left">{{ props.item.nombre }}</td>
+                  <td class="text-xs-left">{{ props.item.descripcion }}</td>
+                  <td> 
+                    <div v-if="props.item.condicion">
+                        <span class="blue--text">Activo</span>
+                    </div>
+                    <div v-else>
+                        <span class="red--text">Inactivo</span>
+                    </div>
+                  </td>
             </template>
             <template slot="no-data">
                 <v-btn color="primary" @click="initialize">Resetear</v-btn>
@@ -90,13 +86,12 @@
           categorias:[],
           dialog: false,
           headers: [
-            { text: 'Opciones', value: 'opciones', sortable: false },
+            { text: 'Opciones', value: 'opciones', align : 'center' ,sortable: false },
             { text: 'Nombres', value: 'nombre' },
             { text: 'Descripción', value: 'descripcion',sorteable: false },
             { text: 'Estado', value: 'condicion',sorteable: false }
           ],
           search: '',
-          desserts: [],
           editedIndex: -1,
           editedItem: {
             name: '',
@@ -116,7 +111,7 @@
     },
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? 'Nueva Categoría' : 'Editar Categoría'
       }
     },
 
@@ -127,7 +122,6 @@
     },
 
     created () {
-      this.initialize();
       this.listar();
     },
 
@@ -138,83 +132,8 @@
           //console.log(response)
           me.categorias=response.data;
         }).catch(function(error){
-          console.log(error)
+          //console.log(error)
         })
-      },
-
-      initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7
-          }
-        ]
       },
 
       editItem (item) {
@@ -247,3 +166,7 @@
     }
   }
 </script>
+<style>
+table th + th { border-left:1px solid #dddddd; }
+table td + td { border-left:1px solid #dddddd; }
+</style>
