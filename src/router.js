469fd1167309a129daf9eb8592513fb9b515fcd9
Martin Ballasco Ramos <martinballasco@gmail.com>
2019-02-22 12:04:08 -0500

    Adding Rol component with List action & refer it to route & main App.vue

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Categoria from './components/Categoria.vue'
 import Articulo from './components/Articulo.vue'
+import Rol from './components/Rol.vue'
 
 
 Vue.use(Router)
@@ -25,6 +26,11 @@ export default new Router({
       path: '/articulos',
       name: 'articulos',
       component : Articulo
+    },
+    {
+      path: '/roles',
+      name: 'roles',
+      component : Rol
     }
   ]
 })
